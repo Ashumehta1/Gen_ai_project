1631439ba1a57830d6fd17f8e53c236e304bba65
Ashish <Ashish@lotuswireless.com>
2025-07-03 16:59:38 +0530

    file uploader

--- file_reader.py
+++ file_reader.py
@@ -1,1 +1,12 @@
 import streamlit as st
+st.header("All file reader")
+
+
+with st.sidebar():
+    st.title("Upload your document here")
+    file_pdf=st.file_uploader("upload pdf", type="pdf")
+    file_text=st.file_uploader("Upload_text", type="text")
+    
+
+    
+
