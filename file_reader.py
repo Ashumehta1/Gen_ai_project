0769e3707bd4e3b2032997a23ffa822d36ae66ca
Ashish <Ashish@lotuswireless.com>
2025-07-03 17:17:56 +0530

    view document by document loader

--- file_reader.py
+++ file_reader.py
@@ -1,4 +1,6 @@
 import streamlit as st
+#from PyPDF2 import PdfReader
+from langchain_community.document_loaders import TextLoader, PyPDFLoader
 st.header("All file reader")
 
 
@@ -7,6 +9,13 @@ with st.sidebar:
     file_pdf=st.file_uploader("upload pdf", type="pdf")
     file_text=st.file_uploader("Upload_text", type="text")
 
+if file_pdf or file_text is not None:
+    loader=TextLoader(file_text)
+    loader_pdf=PyPDFLoader(file_pdf)
+    Text_document=loader.load()
+    pdf_document=loader_pdf.load()
+    st.write(Text_document)
+    st.write(pdf_document)
 
     
 
