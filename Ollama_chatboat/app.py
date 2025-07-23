731011108cf102ec59375b5194de0037652c024f
Ashish <Ashish@lotuswireless.com>
2025-07-23 11:06:47 +0530

    Ollama chatbot 1st commit

---
+++ Ollama_chatboat/app.py
@@ -0,0 +1,6 @@
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_community.llms import Ollama
+import os
+import streamlit as st
+
