1bdd284714e708f2e5193f35bbbb8aff4f9cb7ad
Ashish <Ashish@lotuswireless.com>
2025-07-24 06:32:41 +0530

    used system message

--- Ollama_chatboat/chat_model.py
+++ Ollama_chatboat/chat_model.py
@@ -10,7 +10,18 @@ while True:
     chat_history.append(result)
     print('AI: ',result)
 
-
-
+# Not good for long chat 
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
+chat_history_ = [
+    SystemMessage(content='you are helpful assistant')
+]
+while True:
+    user_input = input("human: ")
+    chat_history.append(HumanMessage(content=user_input))
+    if user_input == 'exit':
+        break
+    result=llm.invoke(chat_history)
+    chat_history.append(AIMessage(content=result))
+    print('AI: ',result)
 
 
